94e30fdb82ac6ccdb57912ae47df95c3aabe0fe5
AsmaeD <asmae.dadi@outlook.com>
2023-03-01 00:11:31 +0100

    adding of test values

--- create_bd_trocencheres.sql
+++ create_bd_trocencheres.sql
@@ -14,7 +14,7 @@ ALTER TABLE CATEGORIES ADD constraint categorie_pk PRIMARY KEY (no_categorie)
 CREATE TABLE ENCHERES (
     no_utilisateur   INTEGER NOT NULL,
     no_article       INTEGER NOT NULL,
-    date_enchere     datetime NOT NULL,
+    date_enchere     INT NOT NULL,
 	montant_enchere  INTEGER NOT NULL
 
 )
@@ -23,9 +23,9 @@ ALTER TABLE ENCHERES ADD constraint enchere_pk PRIMARY KEY (no_utilisateur, no_a
 
 CREATE TABLE RETRAITS (
 	no_article         INTEGER NOT NULL,
-    rue              VARCHAR(30) NOT NULL,
+    rue              VARCHAR(50) NOT NULL,
     code_postal      VARCHAR(15) NOT NULL,
-    ville            VARCHAR(30) NOT NULL
+    ville            VARCHAR(50) NOT NULL
 )
 
 ALTER TABLE RETRAITS ADD constraint retrait_pk PRIMARY KEY  (no_article)
@@ -33,14 +33,14 @@ ALTER TABLE RETRAITS ADD constraint retrait_pk PRIMARY KEY  (no_article)
 CREATE TABLE UTILISATEURS (
     no_utilisateur   INTEGER IDENTITY(1,1) NOT NULL,
     pseudo           VARCHAR(30) NOT NULL,
-    nom              VARCHAR(30) NOT NULL,
-    prenom           VARCHAR(30) NOT NULL,
-    email            VARCHAR(20) NOT NULL,
+    nom              VARCHAR(50) NOT NULL,
+    prenom           VARCHAR(50) NOT NULL,
+    email            VARCHAR(100) NOT NULL,
     telephone        VARCHAR(15),
-    rue              VARCHAR(30) NOT NULL,
+    rue              VARCHAR(50) NOT NULL,
     code_postal      VARCHAR(10) NOT NULL,
-    ville            VARCHAR(30) NOT NULL,
-    mot_de_passe     VARCHAR(30) NOT NULL,
+    ville            VARCHAR(50) NOT NULL,
+    mot_de_passe     VARCHAR(50) NOT NULL,
     credit           INTEGER NOT NULL,
     administrateur   bit NOT NULL
 )
@@ -50,10 +50,10 @@ ALTER TABLE UTILISATEURS ADD constraint utilisateur_pk PRIMARY KEY (no_utilisate
 
 CREATE TABLE ARTICLES_VENDUS (
     no_article                    INTEGER IDENTITY(1,1) NOT NULL,
-    nom_article                   VARCHAR(30) NOT NULL,
+    nom_article                   VARCHAR(50) NOT NULL,
     description                   VARCHAR(300) NOT NULL,
-	date_debut_encheres           DATE NOT NULL,
-    date_fin_encheres             DATE NOT NULL,
+	date_debut_encheres           INT NOT NULL,
+    date_fin_encheres             INT NOT NULL,
     prix_initial                  INTEGER,
     prix_vente                    INTEGER,
     no_utilisateur                INTEGER NOT NULL,
---
+++ insert_RETRAIT.sql
@@ -0,0 +1,16 @@
+USE [TROC_ENCHERES]
+GO
+
+INSERT INTO [dbo].[RETRAITS]
+           ([no_article]
+           ,[rue]
+           ,[code_postal]
+           ,[ville])
+     VALUES
+           (3
+           ,'45 rue jean moulin'
+           ,48000
+           ,'lozere')
+GO
+
+

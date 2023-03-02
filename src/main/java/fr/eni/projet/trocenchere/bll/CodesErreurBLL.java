675e0faec0c10d99d6edc707dc7447883ea34b7d
MickaelThibeault <mickal.thibeault2022@campus-eni.fr>
2023-03-02 12:30:25 +0100

    MODIF - Code BLL (codes erreurs + Manager)

--- src/main/java/fr/eni/projet/trocenchere/bll/CodesErreurBLL.java
+++ src/main/java/fr/eni/projet/trocenchere/bll/CodesErreurBLL.java
@@ -2,4 +2,5 @@ package fr.eni.projet.trocenchere.bll;
 
 public abstract class CodesErreurBLL {
 
+	public static final int REGLE_ATTRIBUT_NON_NULL=20000;
 }
--- src/main/java/fr/eni/projet/trocenchere/bll/TrocEnchereManager.java
+++ src/main/java/fr/eni/projet/trocenchere/bll/TrocEnchereManager.java
@@ -1,5 +1,6 @@
 package fr.eni.projet.trocenchere.bll;
 
+import fr.eni.projet.trocenchere.bo.ArticleVendu;
 
 public class TrocEnchereManager {
 	
@@ -12,22 +13,40 @@ public class TrocEnchereManager {
 	
 	public ArticleVendu ajouter(ArticleVendu articleVendu) throws BusinessException {
 		BusinessException exception = new BusinessException();
+		int maxLenght;
 		
-		Avis avis = new Avis(description, note);
+		// Verification que les certains attributs ne soient pas null
+		this.validerAttributsNotNull(articleVendu,exception);
 		
-		this.validerNote(avis,exception);
-		this.validerDescription(avis,exception);
+		// Verification que la longueur des chaines de caractères ne dépasse pas celle enregistré en base de données
+		this.validerStringLenghtAttributs(articleVendu, maxLenght, exception);
+		
+		// Vérification que les prix soient positifs (ou nuls) et que le prix de vente excéde (ou est égal) le prix initial
+		this.validerPrix(articleVendu);
 
 		if(!exception.hasErreurs())
 		{
-			this.avisDAO.insert(avis);
+			this.trocEnchereDAO.insert(articleVendu);
 		}
 		
 		if(exception.hasErreurs())
 		{
 			throw exception;
 		}
-		return avis;
+		return articleVendu;
+	}
+	
+	private void validerAttributsNotNull (ArticleVendu articleVendu, BusinessException businessException) {
+		
+		if (Integer.valueOf(articleVendu.getNoArticle()) == null || 
+			articleVendu.getNomArticle() == null ||
+			articleVendu.getDescription() == null ||
+			articleVendu.getDateDebutEncheres() == null ||
+			articleVendu.getDateFinEncheres() == null)
+			{
+				businessException.ajouterErreur(CodesErreurBLL.REGLE_ATTRIBUT_NON_NULL);
+			}
+			
 	}
 
 }
